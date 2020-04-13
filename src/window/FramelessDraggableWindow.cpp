69d9be25e566975312802efbd44e044bedff01f6
tbattz <tbatty.tb@gmail.com>
2020-04-13 13:03:26 +1000

    Created a frameless window that may be dragged into position.

---
+++ src/window/FramelessDraggableWindow.cpp
@@ -0,0 +1,52 @@
+//
+// Created by tbatt on 13/04/2020.
+//
+
+#include "FramelessDraggableWindow.h"
+
+namespace GLPL {
+
+    FramelessDraggableWindow::FramelessDraggableWindow(int windowWidth, int windowHeight, bool transparentBackground, bool alwaysOnTop) :
+            GLPL::Window(windowWidth, windowHeight, transparentBackground) {
+        // Set background color
+        this->setFrameless(true);
+        this->setAlwaysOnTop(alwaysOnTop);
+
+        // Setup input callbacks
+        glfwSetMouseButtonCallback(this->getWindow(), mouse_button_callback);
+        glfwSetCursorPosCallback(this->getWindow(), cursor_position_callback);
+
+    }
+
+    void FramelessDraggableWindow::mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+        // Get Window class
+        GLPL::FramelessDraggableWindow* windowPt = (GLPL::FramelessDraggableWindow*) glfwGetWindowUserPointer(window);
+        if (button == GLFW_MOUSE_BUTTON_LEFT) {
+            if (action == GLFW_PRESS) {
+                windowPt->mouseHeld = true;
+                // Update stored cursor position
+                double x, y;
+                glfwGetCursorPos(window, &x, &y);
+                windowPt->cursorX = floor(x);
+                windowPt->cursorY = floor(y);
+            } else if (action == GLFW_RELEASE) {
+                windowPt->mouseHeld = false;
+                windowPt->cursorX = 0;
+                windowPt->cursorY = 0;
+            }
+        }
+    }
+
+    void FramelessDraggableWindow::cursor_position_callback(GLFWwindow *window, double x, double y) {
+        // Get Window class
+        GLPL::FramelessDraggableWindow* windowPt = (GLPL::FramelessDraggableWindow*) glfwGetWindowUserPointer(window);
+        if (windowPt->mouseHeld) {
+            windowPt->xOffset = (int)x - windowPt->cursorX;
+            windowPt->yOffset = (int)y - windowPt->cursorY;
+
+            int winPosX, winPosY;
+            glfwGetWindowPos(window, &winPosX, &winPosY);
+            glfwSetWindowPos(window, winPosX + windowPt->xOffset, winPosY + windowPt->yOffset);
+        }
+    }
+}
---
+++ src/window/FramelessDraggableWindow.h
@@ -0,0 +1,38 @@
+//
+// Created by tbatt on 13/04/2020.
+//
+
+#ifndef OPENGLPLOTLIVE_PROJ_FRAMELESSDRAGGABLEWINDOW_H
+#define OPENGLPLOTLIVE_PROJ_FRAMELESSDRAGGABLEWINDOW_H
+
+// GLM Mathematics
+#include <glm/glm.hpp>
+
+// Project Includes
+#include "window.h"
+
+
+namespace GLPL {
+    class FramelessDraggableWindow : public GLPL::Window {
+    public:
+        // Constructor
+        FramelessDraggableWindow(int windowWidth, int windowHeight, bool transparentBackground,
+                                 bool alwaysOnTop);
+
+    private:
+        // Functions
+        static void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
+        static void cursor_position_callback(GLFWwindow* window, double x, double y);
+
+        // Data
+        bool mouseHeld = false;
+        int cursorX = 0;
+        int cursorY = 0;
+        int xOffset = 0;
+        int yOffset = 0;
+    };
+
+}
+
+
+#endif //OPENGLPLOTLIVE_PROJ_FRAMELESSDRAGGABLEWINDOW_H
