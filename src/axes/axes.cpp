35d2f11954f912d5ddeca5c3c038a579a627270c
tbattz <tbatty.tb@gmail.com>
2021-02-04 19:24:21 +1100

    Adjusting plot spacing.

--- src/axes/axes.cpp
+++ src/axes/axes.cpp
@@ -30,7 +30,7 @@ namespace GLPL {
         // Create Parent Dimensions
         std::shared_ptr<ParentDimensions> newParentPointers = IDrawable::createParentDimensions();
         // Register Child
-        std::shared_ptr<IDrawable> axesAreaObj = std::make_shared<AxesArea>(0.25, 0.2, 0.6, 0.6, newParentPointers);
+        std::shared_ptr<IDrawable> axesAreaObj = std::make_shared<AxesArea>(0.15, 0.1, 0.75, 0.75, newParentPointers);
         std::shared_ptr<AxesArea> axesAreaPt = std::dynamic_pointer_cast<AxesArea>(axesAreaObj);
         Axes::registerChild(axesAreaObj);
         // Store Text String
