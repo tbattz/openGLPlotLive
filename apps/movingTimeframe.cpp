8db45367bc0d8c18a5d5f699e2abd28ee62b2f10
tbattz <tbatty.tb@gmail.com>
2023-01-16 18:57:23 +1100

    First try at getting circular buffers working again for the shaded plots.

--- apps/movingTimeframe.cpp
+++ apps/movingTimeframe.cpp
@@ -23,8 +23,8 @@ int main(int argc, char **argv) {
 	 *                     Setup Window
 	   ====================================================== */
     // Window Size
-    int windowWidth  = 1600;
-    int windowHeight = 800;
+    int windowWidth  = 1800;
+    int windowHeight = 600;
 
 	// Init GLFW
 	std::shared_ptr<GLPL::IWindow> window = std::shared_ptr<GLPL::IWindow>(new GLPL::Window(windowWidth, windowHeight,  false, false));
@@ -58,11 +58,36 @@ int main(int argc, char **argv) {
     yVec13.reserve(2000);
 
 
+    // Graph 32 - Damping
+    std::vector<float> xVec32 = {};
+    std::vector<float> yVec32 = {};
+    xVec32.reserve(2000);
+    yVec32.reserve(2000);
+
+    // Graph 33 - Damping 2
+    std::vector<float> xVec33 = {};
+    std::vector<float> yVec33 = {};
+    xVec33.reserve(2000);
+    yVec33.reserve(2000);
+
+    // Graph 35 - Damping Exponential
+    std::vector<float> xVec35 = {};
+    std::vector<float> yVec35 = {};
+    xVec33.reserve(2000);
+    yVec33.reserve(2000);
+
+    // Graph 36 - Damping Exponential Mirror
+    std::vector<float> xVec36 = {};
+    std::vector<float> yVec36 = {};
+    xVec33.reserve(2000);
+    yVec33.reserve(2000);
+
+
     /* ======================================================
      *                	    Create Plot
        ====================================================== */
 	// Create Plot
-	std::shared_ptr<GLPL::Plot> myplot = std::make_shared<GLPL::Plot>(0.0, 0.0, 1.0, 1.0, window2->getParentDimensions(), 2, 1);
+	std::shared_ptr<GLPL::Plot> myplot = std::make_shared<GLPL::Plot>(0.0, 0.0, 1.0, 1.0, window2->getParentDimensions(), 3, 1);
 	std::shared_ptr<GLPL::IDrawable> myPlotPt = std::dynamic_pointer_cast<GLPL::IDrawable>(myplot);
 	window2->addPlot(myPlotPt);
 
@@ -115,6 +140,30 @@ int main(int argc, char **argv) {
     axes2Pt->setButtonState("Y Axes Limits Scaling", false);
     axes2Pt->showLegend(true);
 
+    // Axes 3 - Spring Damping Live Plot - Manual scale over visible y range, circular buffer shaded plot
+    // Create axes
+    std::shared_ptr<GLPL::Axes2D> axes3Pt = myplot->add2DAxes();
+    // Add Lines
+    std::shared_ptr<GLPL::ILine2D> line32 = axes3Pt->addLine(&xVec32, &yVec32, GLPL::SHADED_LINE, LC_YELLOW, 0.5, "Underdamped 1");
+    std::shared_ptr<GLPL::ILine2D> line33 = axes3Pt->addLine(&xVec33, &yVec33, GLPL::SHADED_LINE, LC_CYAN, 0.5, "Underdamped 2");
+    std::shared_ptr<GLPL::ILine2D> line35 = axes3Pt->addLine(&xVec35, &yVec35, GLPL::SHADED_LINE, LC_BLUE, 0.5, "Upper bounds");
+    std::shared_ptr<GLPL::ILine2D> line36 = axes3Pt->addLine(&xVec36, &yVec36, GLPL::SHADED_LINE, LC_BLUE, 0.5, "Lower Bounds");
+    // Get pointer to add data to internal store
+    std::shared_ptr<GLPL::ShadedLine2D2CircularVecs> line32b = std::dynamic_pointer_cast<GLPL::ShadedLine2D2CircularVecs>(line32);
+    std::shared_ptr<GLPL::ShadedLine2D2CircularVecs> line33b = std::dynamic_pointer_cast<GLPL::ShadedLine2D2CircularVecs>(line33);
+    std::shared_ptr<GLPL::ShadedLine2D2CircularVecs> line35b = std::dynamic_pointer_cast<GLPL::ShadedLine2D2CircularVecs>(line35);
+    std::shared_ptr<GLPL::ShadedLine2D2CircularVecs> line36b = std::dynamic_pointer_cast<GLPL::ShadedLine2D2CircularVecs>(line36);
+    // Set axes configuration
+    axes3Pt->setAxesBoxOn(false);
+    axes3Pt->setButtonState("Grid", false);
+    axes3Pt->setXLabel("Time (s)");
+    axes3Pt->setYLabel("Displacement (m)");
+    axes3Pt->setTitle("Spring Damping Over Time");
+    axes3Pt->setYLabelRotation(GLPL::SIDEWAYS_RIGHT);
+    axes3Pt->setButtonState("X Axes Limits Scaling", true);
+    axes3Pt->setButtonState("Y Axes Limits Scaling", true);
+    axes3Pt->showLegend(true);
+
 
 
     // Create new values
@@ -131,6 +180,7 @@ int main(int argc, char **argv) {
 	 *                     Drawing Loop
 	   ====================================================== */
 	float i = 0;
+	float k = 0;
 
 	// Game Loop
 	while(!glfwWindowShouldClose(window->getWindow())) {
@@ -138,7 +188,7 @@ int main(int argc, char **argv) {
 		// Pre-loop draw
 		window2->preLoopDraw(true);
 
-		// Update Plot Data
+		// Update Plot 1 Data
         // Update graph 12
         line12b->dataPtX->push_back(i);
         yVal12 = cos((i) / (25*M_PI)) * exp(-(i)/(25*8*M_PI));
@@ -164,7 +214,7 @@ int main(int argc, char **argv) {
         line16b->updateInternalData();
 
 
-        // Update Plot Data
+        // Update Plot 2 Data
         // Update graph 22
         line22b->updateInternalData();
 
@@ -179,7 +229,34 @@ int main(int argc, char **argv) {
 
 
 
+
+        // Update Plot 3 Data
+        // Update graph 32
+        line32b->dataPtX->push_back(i);
+        line32b->dataPtY->push_back(yVal12);
+        line32b->updateInternalData(0);
+
+        // Update graph 33
+        line33b-> dataPtX->push_back(i);
+        line33b->dataPtY->push_back(yVal13);
+        line33b->updateInternalData(0);
+
+        // Update Graph 35
+        line35b->dataPtX->push_back(i);
+        line35b->dataPtY->push_back(yVal15);
+        line35b->updateInternalData(0);
+
+        // Update Graph 36
+        line36b->dataPtX->push_back(i);
+        line36b->dataPtY->push_back(yVal16);
+        line36b->updateInternalData(0);
+
+
+
         i += 1;
+        k += 1;
+
+        if (k > 1000) { k-= 1000; }
 
         // Get range of last 1000 points, provided the points in the a axis are in order
         unsigned int minInd = std::max((long)0, (long)line12b->dataPtX->size() - 1000);
@@ -188,6 +265,7 @@ int main(int argc, char **argv) {
         float xmax = (*line12b->dataPtX)[maxInd];
         axesPt->setXAxesLimits(xmin, xmax);
         axes2Pt->setXAxesLimits(xmin, xmax);
+        //axes3Pt->setXAxesLimits(xmin, xmax);
 
 
         // Handle y axes for second plot
@@ -204,6 +282,7 @@ int main(int argc, char **argv) {
             }
         }
         axes2Pt->setYAxesLimits(ymin, ymax);
+        //axes3Pt->setYAxesLimits(ymin, ymax);
 
 
 
